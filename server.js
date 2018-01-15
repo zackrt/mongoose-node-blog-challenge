db52458a9037244674cec00354597b2fcfffe679
Zack Toth <zacharyrtoth@gmail.com>
2018-01-15 09:47:08 -0800

    edited server.js

--- server.js
+++ server.js
@@ -17,3 +17,17 @@ const app = express();
 //call morgan & bodyParser.json
 app.use(morgan('common'));
 app.use(bodyParser.json());
+
+
+app.get('/posts', (req, res) => {
+    //create BlogPost promise
+    BlogPost
+      .find()
+      .then(posts => {
+        res.json(posts.map(post => post.serialize()));
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'something went terribly wrong' });
+      });
+  });
